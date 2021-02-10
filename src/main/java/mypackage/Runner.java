be38d39eea60f7befc7e3c3273e975897d997ee5
Paul Kline <pauliankline@gmail.com>
2021-02-10 09:21:18 -0600

    first commit

---
+++ src/main/java/mypackage/Runner.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackage;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paul
+ */
+public class Runner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
+    }
+
+}
